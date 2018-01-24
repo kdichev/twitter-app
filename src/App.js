872fc82e28dd595a98aa659b6c597ef276519805
Konstantin Dichev <konstantin.dichev@gmail.com>
2018-01-24 09:56:46 +0100

    clean up cra

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from './components/AppBar'
+import Field from './components/Field'
+import Button from './components/Button'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <div>
+        <AppBar>
+          <Field onChange={() => console.log('onChange')}/>
+          <Button onClick={() => console.log('Click')}>Search</Button>
+        </AppBar>
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
